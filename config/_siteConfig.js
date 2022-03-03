4a235aee270218156e5461758bb30d3120ac60a1
zxdc8 <oliver.c.cairns@gmail.com>
2022-03-03 19:05:10 +0000

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -9,8 +9,8 @@ import theCMS from '../cms/netlify/cms'
  * General Site configurations
  */
 export default {
-  siteName: 'Awake',
-  tagline: 'A Nuxt.js Theme for Netlify CMS',
+  siteName: 'Oliver Cairns',
+  tagline: 'A place for my things',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
